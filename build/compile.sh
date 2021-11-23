dce15406fce49bb1639d583a1a2f189a90592d17
benjamin-grill <benjamin_grill@gmx.at>
2021-11-23 20:00:15 +0100

    add mkdir

--- build/compile.sh
+++ build/compile.sh
@@ -5,6 +5,8 @@ read -r VERSION < ./build/version
 SOURCE="./source/w3b.scss"
 TARGET="./css"
 
+mkdir $TARGET
+
 sass $SOURCE $TARGET/w3b-$VERSION.css
 sass --style=compressed $SOURCE $TARGET/w3b-$VERSION.min.css
 

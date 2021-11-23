e8090a4ff6008b7eb5e03b0fff245e1bf93a296f
benjamin-grill <benjamin_grill@gmx.at>
2021-11-23 19:53:18 +0100

    update paths

--- build/compile.sh
+++ build/compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-read -r VERSION < version
+read -r VERSION < ./version
 
-SOURCE="../source/w3b.scss"
-TARGET="../css"
+SOURCE="./source/w3b.scss"
+TARGET="./css"
 
 sass $SOURCE $TARGET/w3b-$VERSION.css
 sass --style=compressed $SOURCE $TARGET/w3b-$VERSION.min.css
